ad850d7b1910485668ce997e4a944e45f7257722
Adam Viberg <66957534+adam-viberg@users.noreply.github.com>
2023-02-17 02:04:06 +0100

    Enabled AVX abd implemeted cross product usingg SIMD

--- Source Files/Vector3.cpp
+++ Source Files/Vector3.cpp
@@ -1,5 +1,9 @@
 #include "Vector3.hpp"
 #include <cmath>
+#include <immintrin.h>
+
+constexpr int SHUFFLE_MASK_0 = _MM_SHUFFLE(0, 2, 1, 0);
+constexpr int SHUFFLE_MASK_1 = _MM_SHUFFLE(0, 0, 2, 1);
 
 Vector3::Vector3() : x(0), y(0), z(0) {}
 Vector3::Vector3(double x, double y, double z) : x(x), y(y), z(z) {}
@@ -14,5 +18,18 @@ Vector3 Vector3::operator-() const { return Vector3(-x, -y, -z); }
 double Vector3::dot(const Vector3 &v) const { return x * v.x + y * v.y + z * v.z; }
 double Vector3::length() const { return sqrt(x * x + y * y + z * z); }
 Vector3 Vector3::normalize() const { return (*this) / length(); }
-Vector3 Vector3::cross(const Vector3 &v) const { return Vector3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+Vector3 Vector3::cross(const Vector3 &v) const {
+    __m256d vec0 = _mm256_set_pd(x, y, z, 0);
+    __m256d vec1 = _mm256_set_pd(v.x, v.y, v.z, 0);
+
+    __m256d a_yzx = _mm256_shuffle_pd(vec0, vec0, SHUFFLE_MASK_0);
+    __m256d b_yzx = _mm256_shuffle_pd(vec1, vec1, SHUFFLE_MASK_0);
+
+    __m256d a_zxy = _mm256_shuffle_pd(vec0, vec0, SHUFFLE_MASK_1);
+    __m256d b_zxy = _mm256_shuffle_pd(vec1, vec1, SHUFFLE_MASK_1);
+
+    __m256d result = _mm256_sub_pd(_mm256_mul_pd(a_yzx, b_zxy), _mm256_mul_pd(b_yzx, a_zxy));
+
+    return Vector3(result[0], result[1], result[2]);
+}
 Vector3 Vector3::avarage(const Vector3 &v) const { return (*this + v) / 2; }
